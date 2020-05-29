531ce8825baa059d0b02bf3d81d21416158b4f7b
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-05-30 00:40:52 +0530

    Create AsciiValue.java

---
+++ Day3/Arrays/AsciiValue.java
@@ -0,0 +1,19 @@
+//Initialize an integer array with ascii values and print the corresponding character values in a single row. 
+
+
+package project;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+			        int num[] = {65, 120, 98, 75, 115};
+			        char s;
+			        for(int i: num){
+			            s = (char)i;
+			            System.out.println(s);
+			        }
+		}
+
+	}
+
+
