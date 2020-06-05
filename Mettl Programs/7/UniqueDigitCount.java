1791d5ee4011909ecd62155fe43a53f43e5f0fde
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-06-05 13:37:40 +0530

    Update UniqueDigitCount.java

--- Mettl Programs/7/UniqueDigitCount.java
+++ Mettl Programs/7/UniqueDigitCount.java
@@ -4,3 +4,18 @@ class UniqueDigitCount
 {
 	public int distinctDigitCount(int input1)
 	{
+		ArrayList<Integer> a=new ArrayList<Integer>(5);
+		int count=0,d;
+		while(input1!=0)
+		{	
+			d=input1%10;
+			if(!a.contains(d))
+			{
+				a.add(d);
+				count++;
+			}
+			input1/=10;
+		}
+		return count;
+	}
+}
