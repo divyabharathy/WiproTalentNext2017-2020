9e18aa8d9e204c055a782b556a8902052101599f
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-06-05 13:21:36 +0530

    Update AllDigitsCount.java

--- Mettl Programs/7/AllDigitsCount.java
+++ Mettl Programs/7/AllDigitsCount.java
@@ -2,5 +2,14 @@ import java.io.*;
 import java.util.*;
 class AllDigitsCount
 {
-	public int countPrimes(int input1,int input2)
+	public int digitCount(int input1)
 	{
+		int count=0;
+    		while(input1!=0)
+    		{
+        		count++;
+        		input1/=10;
+    		}
+    		return count;
+	}
+}
