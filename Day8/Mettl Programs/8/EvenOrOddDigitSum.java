ed9adec0a83810117b1b555d87a0d4a16dc71009
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-06-05 19:36:07 +0530

    Update EvenOrOddDigitSum.java

--- Day8/Mettl Programs/8/EvenOrOddDigitSum.java
+++ Day8/Mettl Programs/8/EvenOrOddDigitSum.java
@@ -4,3 +4,27 @@ class EvenOrOddDigitSum
 {
 	public int eveOddSum(int input1,String input2)
 	{
+		int odd=0,even=0,d;
+    		while(input1!=0)
+   	 	{
+        		d=input1%10;
+        		if(d%2!=0)
+        		{
+           			odd+=d;
+        		}
+				else
+				{
+					even+=d;
+				}
+        		input1/=10;
+    		}
+    		if(input2.equalsIgnoreCase("even"))
+			{
+				return even;
+			}
+			else
+			{
+				return odd;
+			}
+	}
+}
