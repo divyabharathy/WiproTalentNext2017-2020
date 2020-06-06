dab7e15ad3e8c8d07526492a074d0bd7e5aaa255
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-06-06 08:45:31 +0530

    Create WeightOfHillPattern.java

---
+++ Day8/Mettl Programs/9/WeightOfHillPattern.java
@@ -0,0 +1,14 @@
+class WeightOfHillPattern
+{
+  public int weightOfPattern(int input1,int input2,int input3)
+  {
+    int i,sum=0;
+    for(i=1;i<=input1;i++)
+    {
+        sum+=input2*i;
+        input2+=input3;
+
+    }
+    return sum;
+  }
+}
