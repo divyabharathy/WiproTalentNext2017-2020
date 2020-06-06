de711f074cd14130d5fe952cba265bf6094a8cd3
divyabharathy <60822129+divyabharathy@users.noreply.github.com>
2020-06-06 19:09:47 +0530

    Update WeightOfString.java

--- Day8/Mettl Programs/10/WeightOfString.java
+++ Day8/Mettl Programs/10/WeightOfString.java
@@ -4,3 +4,23 @@ class WeightOfString
 {
 	public String stringWeight(String input1)
 	{
+		int t=0,v=0;
+		input1=input1.toUpperCase();
+		for(int i=0;i<input1.length();i++)
+		{
+			if(input1.charAt(i)=='A'||input1.charAt(i)=='E'||input1.charAt(i)=='I'||input1.charAt(i)=='O'||input1.charAt(i)=='U')
+			{
+				v+=(int)input1.charAt(i)-64;
+			}
+			else if(input1.charAt(i)>='A'&&input1.charAt(i)<='Z')
+			{
+				t+=(int)input1.charAt(i)-64;
+			}
+		}
+		if(input2==0)
+		{
+			return t;
+		}
+		return t+v;
+	}
+}
