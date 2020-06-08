0a7246e1ccc238a776351a63330040d19f081e26
Divya Bharathy Venkatraman <60822129+divyabharathy@users.noreply.github.com>
2020-06-09 00:41:57 +0530

    Update CodethrStrings.java

--- Day9/String Based Programs/CodethrStrings.java
+++ Day9/String Based Programs/CodethrStrings.java
@@ -4,3 +4,22 @@ class CodethrStrings
 {
   public int toCodeStrings(String input1)
   {
+		int count=0,val=0;;
+		input1=input1.toUpperCase();
+		String[] s=input1.split(" ");
+		for(int i=0;i<s.length;i++)
+		{
+			count+=s[i].length();
+		}
+		while(count/10!=0)
+		{
+			while(count!=0)
+			{
+				val+=count%10;
+				count/=10;
+			}	
+			count=val;
+		}
+		return count;
+	}
+}
