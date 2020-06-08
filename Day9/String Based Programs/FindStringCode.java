dbae71b93a26ba26a184adbd9f160fe4d87679c1
Divya Bharathy Venkatraman <60822129+divyabharathy@users.noreply.github.com>
2020-06-08 22:23:22 +0530

    Update FindStringCode.java

--- Day9/String Based Programs/FindStringCode.java
+++ Day9/String Based Programs/FindStringCode.java
@@ -4,4 +4,33 @@ class FindStringCode
 {
   public int toStringCode(String input1)
   {
+		int temp=0,count,l=0;
+		String val="";
+		input1=input1.toUpperCase();
+		String[] s=input1.split(" ");
+		for(int i=0;i<s.length;i++)
+		{
+			l=0;
+			temp=0;
+			l=s[i].length()-1;
+			if(s[i].length()==1)
+			{
+				temp=(int)(s[i].charAt(0))-64;	
+			}
+		    for(int j=0;j<s[i].length()/2;j++)
+		    {
+				
+		    	if(s[i].length()%2!=0&&j==s[i].length()/2-1)
+		    	{
+		    		temp+=(int)(s[i].charAt(j+1))-64;
+		    	}
+		       temp+=Math.abs(((int)(s[i].charAt(j))-64)-((int)(s[i].charAt(l))-64));
+		       l--;
+		    }
+		   val+=String.valueOf(temp);
+		}
+		count=Integer.parseInt(val);
+		return count;
+	}
+}
     
